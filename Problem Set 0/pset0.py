cf9ad2d0b4e11875d4e361569e73a8ff29c9e02e
abode118 <46899622+abode118@users.noreply.github.com>
2019-08-25 15:08:24 -0400

    Create pset0.py

---
+++ Problem Set 0/pset0.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 20:24:14 2018
+
+@author: abode118
+"""
+import numpy
+
+number_x = input("Enter number x: ")
+number_y = input("Enter number y: ")
+print("x ** y =", int(number_x) ** int(number_y))
+print("log(x) =", numpy.log2(int(number_x)))
+
+input("My name is ")
+input("My sign is ")
+input("My number is ")
+print("you need to let it go")
